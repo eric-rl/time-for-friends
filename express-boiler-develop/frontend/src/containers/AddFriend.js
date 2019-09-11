3bf567f5a25620417e4a0c7b800365c0a2cb9672
Johan Schultze <Johan.schultze@gmail.com>
2019-09-11 16:44:23 +0200

    Implement react router, working with 3 pages.

---
+++ express-boiler-develop/frontend/src/containers/AddFriend.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddFriend extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ADD FRIEND</h1>
+            </div>
+        )
+    }
+}
+
+export default AddFriend;
