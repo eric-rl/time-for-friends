33f7a7dde1741614ff0570cf71e6b40fa2f69816
Johan Schultze <Johan.schultze@gmail.com>
2019-09-13 19:22:03 +0200

    Minor color changes

--- express-boiler-develop/frontend/src/components/Navigationbar.js
+++ express-boiler-develop/frontend/src/components/Navigationbar.js
@@ -6,14 +6,14 @@ class Navigationbar extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="dark" variant="dark" expand="lg">
-                    <Navbar.Brand href="/">Time for friends</Navbar.Brand>
+                <Navbar fixed="top" className="blabla" bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand>Time for friends</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <NavLink className="nav-link" to="/" style={{color: 'rgba(255,255,255,.5)', textDecoration: 'none'}}>Home</NavLink>
-                            <NavLink className="nav-link" to="/friends" style={{color: 'rgba(255,255,255,.5)', textDecoration: 'none'}}>Friends</NavLink>
-                            <NavLink className="nav-link" to="/add-friend" style={{color: 'rgba(255,255,255,.5)', textDecoration: 'none'}}>Add friend</NavLink>
+                            <NavLink className="nav-link" to="/" >Home</NavLink>
+                            <NavLink className="nav-link" to="/friends">Friends</NavLink>
+                            <NavLink className="nav-link" to="/add-friend">Add friend</NavLink>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
