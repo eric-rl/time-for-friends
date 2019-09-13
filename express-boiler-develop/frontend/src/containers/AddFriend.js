352601f62eae1a983543f10919c55a83dc8a07e7
Johan Schultze <Johan.schultze@gmail.com>
2019-09-13 17:09:06 +0200

    WIP: Posting a new friend

--- express-boiler-develop/frontend/src/containers/AddFriend.js
+++ express-boiler-develop/frontend/src/containers/AddFriend.js
@@ -3,13 +3,43 @@ import { Form, Button } from 'react-bootstrap'
 
 export default class AddFriend extends Component {
 
-    dataCheck() {
-        console.log("i dataCheck");
+    constructor(props) {
+        super(props)
+        this.firstName = React.createRef();
+        this.lastName = React.createRef();
+        this.email = React.createRef();
+        this.phoneNumber = React.createRef();
+        this.timezone = React.createRef();
+        this.country = React.createRef();
+        this.city = React.createRef();
     }
 
-    componentDidUpdate(){
-        console.log(this.inputName.value);
+    async dataCheck() {
+        let data = {
+            name: {
+                firstName: this.firstName.current.value,
+                lastName: this.lastName.current.value
+            },
+            location: {
+                timezone: this.timezone.current.value,
+                country: this.country.current.value,
+                city: this.city.current.value
+            },
+            email: this.email.current.value,
+            phoneNumber: this.phoneNumber.current.value
+        }
+
+        let result = await fetch('/api/person', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: data
+        });
+
+        console.log(result)
+        console.log(data)
     }
+
+
 
     render() {
         return (
@@ -17,31 +47,31 @@ export default class AddFriend extends Component {
                 <Form>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>First name</Form.Label>
-                        <Form.Control type="text" placeholder="Åke" />
+                        <Form.Control type="text" placeholder="Åke" ref={this.firstName} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Last name</Form.Label>
-                        <Form.Control type="text" placeholder="Torsson" />
+                        <Form.Control type="text" placeholder="Torsson" ref={this.lastName} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="åke.torsson.cool@gmail.com" />
+                        <Form.Control type="email" placeholder="åke.torsson.cool@gmail.com" ref={this.email} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Phone number</Form.Label>
-                        <Form.Control type="number" placeholder="123-456789" />
+                        <Form.Control type="number" placeholder="123-456789" ref={this.phoneNumber} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Country</Form.Label>
-                        <Form.Control type="text" placeholder="Sweden" />
+                        <Form.Control type="text" placeholder="Sweden" ref={this.country} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>City</Form.Label>
-                        <Form.Control type="text" placeholder="Lund" />
+                        <Form.Control type="text" placeholder="Lund" ref={this.city} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlSelect1">
                         <Form.Label>Timezone</Form.Label>
-                        <Form.Control as="select">
+                        <Form.Control as="select" ref={this.timezone} >
                             <option>Europe/Oslo</option>
                             <option>Africa/Accra</option>
                             <option>America/New_York</option>
@@ -49,7 +79,7 @@ export default class AddFriend extends Component {
                             <option>America/Montreal</option>
                         </Form.Control>
                     </Form.Group>
-                    <Button onClick={this.dataCheck} variant="primary">
+                    <Button onClick={() => this.dataCheck()} variant="primary">
                         Submit
                     </Button>
                 </Form>
