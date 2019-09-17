ef7022e6cfc54ca1b27d4c33e68ba8a3bbab67ee
Eric Rasmusson Lindström <eric.rl@me.com>
2019-09-17 15:27:57 +0200

    Marker on map WIP

--- express-boiler-develop/frontend/src/components/FriendMap.js
+++ express-boiler-develop/frontend/src/components/FriendMap.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react'
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 class FriendMap extends Component {
     state = {
-        points: {}
+        points: [],
+        loading: true,
+        data: []
     }
     async componentDidMount() {
-        let points = await (await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=London&key=AIzaSyB4pPsphC1Am-jN9AYwCaUZ3gYsDnSSOtE")).json()
-        this.setState({points})
-        console.log(this.state.points);
+        this.getData()
+        
+        // console.log(this.state.points.results[0].geometry.location);
+    }
+
+    async getData(){
+        // let data = await (await fetch("/api/persons"))
+        let points = await (await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=Lomma&key=AIzaSyB4pPsphC1Am-jN9AYwCaUZ3gYsDnSSOtE")).json()
+        this.setState({ points, loading: false })
+        // this.setState({ data})
+        // console.log(this.state.data)
     }
 
     render() {
+        if (!this.state.loading) {
+            console.log(this.state.points.results[0].address_components[0].long_name);
+        }
         const mapStyles = {
             width: '84%',
-            height: '69%',
+            height: '79%',
             marginLeft: 'auto',
             marginRight: 'auto',
         };
 
+
         return (
             <div className="map-container">
-                <Map className="map-map"
-                    google={this.props.google}
-                    zoom={5}
-                    style={mapStyles}
-                    initialCenter={{ lat: 0.0, lng: 0.0 }}
-                    // disableDefaultUI= {true}
-                    mapTypeId={'satellite'}
-                />
+                {this.state.loading ? <h1> asd </h1> :
+                    <Map
+                        className="map-map"
+                        google={this.props.google}
+                        zoom={2}
+                        style={mapStyles}
+                        initialCenter={{ lat: 30, lng: 1 }}
+                        disableDefaultUI= {true}
+                    >
+                        <Marker 
+                        title={this.state.points.results[0].address_components[0].long_name}
+                        name={'hello'}
+                        position={{
+                            lat: this.state.points.results[0].geometry.location.lat,
+                            lng: this.state.points.results[0].geometry.location.lng
+                        }}
+                        ></Marker>
+                    </Map>
+                }
             </div>
         )
     }
