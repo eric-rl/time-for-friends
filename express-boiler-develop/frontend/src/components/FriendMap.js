da5ab6ee3201997aaee8b6f2bfe354d94e64cd46
Eric Rasmusson Lindström <eric.rl@me.com>
2019-09-18 15:31:59 +0200

    Bit more google-map work

--- express-boiler-develop/frontend/src/components/FriendMap.js
+++ express-boiler-develop/frontend/src/components/FriendMap.js
@@ -9,65 +9,51 @@ class FriendMap extends Component {
     }
     async componentDidMount() {
         await this.getData()
-
-        // console.log(this.state.points.results[0].geometry.location);
     }
 
     async getData() {
         let data = await (await fetch("/api/person")).json()
-        // let points = await (await fetch("https://maps.googleapis.com/maps/api/geocode/json?address=Lomma&key=AIzaSyB4pPsphC1Am-jN9AYwCaUZ3gYsDnSSOtE")).json()
-        // this.setState({ points, loading: false })
         this.setState({ data })
-        console.log(this.state.data)
 
-        let p
+
         let getMarkerPoints = []
-        for (p of this.state.data) {
+        for (let p of this.state.data) {
             getMarkerPoints.push(await (await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${p.location.city}&${p.location.country}&key=AIzaSyB4pPsphC1Am-jN9AYwCaUZ3gYsDnSSOtE`)).json())
-            console.log(getMarkerPoints)
         }
         this.setState({ loading: false, markerPoints: getMarkerPoints })
         console.log(this.state.markerPoints)
     }
 
     render() {
-        // if (!this.state.loading) {
-        //     console.log(this.state.points.results[0].address_components[0].long_name);
-        // }
-        const mapStyles = {
-            width: '84%',
-            height: '79%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        };
-
-
         return (
-            <div className="map-container">
-                <Map
-                    className="map-map"
-                    google={this.props.google}
-                    zoom={2}
-                    style={mapStyles}
-                    initialCenter={{ lat: 30, lng: 1 }}
-                    disableDefaultUI={true}
-                >
-                    {this.state.loading ? (<h1> Loading </h1>) :
-                        (
-                            this.state.markerPoints.map(city => (
+            <div className="col-10 offset-1">
+                <hr />
+                <h1 className='f1 tc'>Friend Map</h1>
+                <div className="map-container">
+                    <Map
+                        className="map-map"
+                        google={this.props.google}
+                        zoom={2}
+                        initialCenter={{ lat: 20, lng: 1 }}
+                        disableDefaultUI={true}
+                    >
+                        {this.state.loading ? (<h1 className="loading"> Loading </h1>) :
+                            (
+                                this.state.markerPoints.map(city => (
 
-                                <Marker
-                                    key={city.results}
-                                    title={city.results[0].address_components[0].long_name}
-                                    position={{
-                                        lat: city.results[0].geometry.location.lat,
-                                        lng: city.results[0].geometry.location.lng
-                                    }}
-                                ></Marker>
-                            ))
-                        )
-                    }
-                </Map>
+                                    <Marker
+                                        key={this.state.markerPoints.indexOf(city)}
+                                        title={city.results[0].address_components[0].long_name}
+                                        position={{
+                                            lat: city.results[0].geometry.location.lat,
+                                            lng: city.results[0].geometry.location.lng
+                                        }}
+                                    ></Marker>
+                                ))
+                            )
+                        }
+                    </Map>
+                </div>
             </div>
         )
     }
