1bd74fa2b4a25f8bacb44ed67b176b1a7a2c5ec6
Johan Schultze <Johan.schultze@gmail.com>
2019-09-26 15:28:02 +0200

    Implementing store i Friends typ klar

--- express-boiler-develop/frontend/src/containers/Friends.js
+++ express-boiler-develop/frontend/src/containers/Friends.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputGroup, FormControl, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import FriendCard from '../components/FriendCard';
 import InputRange from 'react-input-range';
@@ -12,31 +12,25 @@ import '../css/search.css'
 export default function Friends(props) {
 
     const { state, dispatch } = React.useContext(Store);
-    // const [search, setSearch] = useState('')
-    // const [rangeValue, setRangeValue] = useState({ min: 0, max: 23 })
-    // const [sortByFirstName, setSortByFirstName] = useState(true)
-    // const [sortValue, setSortValue] = useState(1)
+    const [search, setSearch] = useState('')
+    const [rangeValue, setRangeValue] = useState({ min: 0, max: 23 })
+    const [sortValue, setSortValue] = useState(1)
     const [sortedAndFilteredData, setSortedAndFilteredData] = useState([])
 
     useEffect(() => {
         state.friends.length === 0 && fetchDataAction();
+        setSortedAndFilteredData(state.friends.sort((a, b) => a.name.firstName.localeCompare(b.name.firstName)))
+        console.log(sortedAndFilteredData)
     });
-    
-    console.log(state.friends)
 
-    // function handleChange(e) {
-    //     this.setState({ search: e.target.value.toLowerCase() })
-    // }
+    function handleChange(e) {
+        setSearch(e.target.value.toLowerCase())
+    }
 
-    // function handleToggleChange(e) {
-    //     this.setState({ sortValue: e })
-    //     this.sortFriends(e)
-    // }
-
-    // React.useEffect(() => {
-    //     setSortedAndFilteredData(state.friends)
-    //     state.friends.length === 0 && fetchDataAction();
-    // });
+    function handleToggleChange(e) {
+        setSortValue(e)
+        sortFriends(e)
+    }
 
     const fetchDataAction = async () => {
         const data = await fetch("/api/person");
@@ -47,37 +41,37 @@ export default function Friends(props) {
         });
     };
 
-    // function sortFriends(e) {
-    //     switch (e) {
-    //         case 1:
-    //             this.setState({ sortedAndFilteredData: this.state.sortedAndFilteredData.sort((a, b) => a.name.firstName.localeCompare(b.name.firstName)) })
-    //             break;
-    //         case 2:
-    //             this.setState({ sortedAndFilteredData: this.state.sortedAndFilteredData.sort((a, b) => a.name.lastName.localeCompare(b.name.lastName)) })
-    //             break;
-    //         case 3:
-    //             this.setState({ sortedAndFilteredData: this.state.sortedAndFilteredData.sort((a, b) => a.location.timezone.localeCompare(b.location.timezone)) })
-    //             break;
-    //         default:
-    //             return
-    //     }
-    // }
+    function sortFriends(e) {
+        switch (e) {
+            case 1:
+                setSortedAndFilteredData(sortedAndFilteredData.sort((a, b) => a.name.firstName.localeCompare(b.name.firstName)))
+                break;
+            case 2:
+                setSortedAndFilteredData(sortedAndFilteredData.sort((a, b) => a.name.lastName.localeCompare(b.name.lastName)))
+                break;
+            case 3:
+                setSortedAndFilteredData(sortedAndFilteredData.sort((a, b) => a.location.timezone.localeCompare(b.location.timezone)))
+                break;
+            default:
+                return
+        }
+    }
 
-    // const filteredData = sortedAndFilteredData.filter(friend =>
-    //     (friend.name.firstName.toLowerCase().startsWith(this.state.search) ||
-    //         friend.name.lastName.toLowerCase().startsWith(this.state.search)) &&
-    //     Moment.tz(new Date(), friend.location.timezone).format("HH") >= this.state.rangeValue.min &&
-    //     Moment.tz(new Date(), friend.location.timezone).format("HH") < this.state.rangeValue.max);
+    const filteredData = sortedAndFilteredData.filter(friend =>
+        (friend.name.firstName.toLowerCase().startsWith(search) ||
+            friend.name.lastName.toLowerCase().startsWith(search)) &&
+        Moment.tz(new Date(), friend.location.timezone).format("HH") >= rangeValue.min &&
+        Moment.tz(new Date(), friend.location.timezone).format("HH") < rangeValue.max);
 
     return (
         <div className="col-12 col-sm-10 offset-sm-1 mb-5">
-            {/* <h1 className='f1 tc'>My Friends</h1>
+            <h1 className='f1 tc'>My Friends</h1>
             <div className="col-10 offset-1">
                 <div className="d-flex row justify-content-around">
                     <InputGroup size="md" className="col-12 col-sm-4 p-0" >
-                        <FormControl placeholder="Search..." onKeyUp={this.handleChange} aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+                        <FormControl placeholder="Search..." onKeyUp={handleChange} aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
                     </InputGroup>
-                    <ToggleButtonGroup name="hej" type="radio" value={this.state.sortValue} onChange={this.handleToggleChange}>
+                    <ToggleButtonGroup name="hej" type="radio" value={sortValue} onChange={handleToggleChange}>
                         <ToggleButton value={1}>First name</ToggleButton>
                         <ToggleButton value={2}>Last name</ToggleButton>
                         <ToggleButton value={3}>Timezone</ToggleButton>
@@ -86,15 +80,15 @@ export default function Friends(props) {
                 <InputRange
                     maxValue={23}
                     minValue={0}
-                    value={this.state.rangeValue}
-                    onChange={value => this.setState({ rangeValue: value })} />
+                    value={rangeValue}
+                    onChange={value => setRangeValue( value ) } />
             </div>
             <div className="tc" >
                 {
                     filteredData.map(item => <FriendCard className="friend-card" key={item._id}{...item}>
                     </FriendCard>)
                 }
-            </div> */}
+            </div>
         </div>
     )
 
