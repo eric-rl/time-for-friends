c05be7b4d22fa2f3b4a1aca0c38adf664901c5a4
Eric Rasmusson Lindström <eric.rl@me.com>
2019-10-03 12:40:12 +0200

    Hiding navigation links when not logged in

--- express-boiler-develop/frontend/src/components/Navigationbar.js
+++ express-boiler-develop/frontend/src/components/Navigationbar.js
@@ -16,17 +16,21 @@ export default function Navigationbar(props) {
     return (
         <div className="d-none d-sm-block">
             <div className="flex justify-center">
-                <NavLink className="nav-link" to="/" >Home</NavLink>
-                <NavLink className="nav-link" to="/friends">Friends</NavLink>
-                <NavLink className="nav-link" to="/add-friend">Add friend</NavLink>
+                {
+                    state.isLoggedIn ? <NavLink className="nav-link" to="/" >Home</NavLink> : <p></p>
+                }
+                {
+                    state.isLoggedIn ? <NavLink className="nav-link" to="/friends">Friends</NavLink> : <p></p>
+                }
+                {
+                    state.isLoggedIn ? <NavLink className="nav-link" to="/add-friend">Add friend</NavLink> : <p></p>
+                }
                 {
                     state.currentUser && state.currentUser.name ? <NavLink className="nav-link" to="/friends">{state.currentUser.name}</NavLink> : <p></p>
                 }
                 {
                     state.isLoggedIn ? <div className="nav-link" onClick={logout}>Logout</div> : <NavLink className="nav-link" to="/login">Login</NavLink>
                 }
-
-
             </div>
             <hr className="col-10 offset-1" />
         </div>
