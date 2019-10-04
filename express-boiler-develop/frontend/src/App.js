c6d1ea00e2a1f255427006c8cc90244885f7ee71
Johan Schultze <Johan.schultze@gmail.com>
2019-10-04 11:46:32 +0200

    WIP redirect on reload

--- express-boiler-develop/frontend/src/App.js
+++ express-boiler-develop/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import FriendMap from './components/FriendMap';
 import Friends from './containers/Friends';
@@ -9,6 +9,7 @@ import NavigationMobile from './components/NavigationbarMobile'
 import FriendDetails from './containers/FriendDetails';
 import Register from './containers/Register';
 import Login from './containers/Login';
+import Loading from './containers/Loading.js'
 import PrivateRoutes from './utilities/PrivateRoutes';
 import { Store } from './utilities/Store';
 
@@ -51,8 +52,9 @@ export default function App() {
         <PrivateRoutes exact path="/friends" component={Friends} isAuthenticated={state.isLoggedIn} redirectPath="/login" />
         <PrivateRoutes path="/add-friend" component={AddFriend} isAuthenticated={state.isLoggedIn} redirectPath="/login" />
         <PrivateRoutes path="/friend/:id" component={FriendDetails} isAuthenticated={state.isLoggedIn} />
-        <PrivateRoutes path="/register" component={Register} isAuthenticated={!state.isLoggedIn} redirectPath="/" />
-        <PrivateRoutes path="/login" component={Login} isAuthenticated={!state.isLoggedIn} redirectPath="/" />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        {/* <Route path="/loading" component={Loading} /> */}
       </div>
     </Router>
   );
--- express-boiler-develop/frontend/src/utilities/PrivateRoutes.js
+++ express-boiler-develop/frontend/src/utilities/PrivateRoutes.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect, Route } from 'react-router-dom'
+import { Store } from '../utilities/Store';
 
 const PrivateRoutes = props => {
+
     const { component: Component, isAuthenticated, redirectPath, ...rest } = props;
+    const { state, dispatch } = React.useContext(Store);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        // checkLoginStatus()
+      });
 
     return (
         <Route
